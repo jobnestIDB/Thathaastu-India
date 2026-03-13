6b0c54511e850714187e7de4bf5321c9d82517b5
shreya <shreya@gmail.com>
2026-03-13 11:41:18 +0530

    Updated code

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ window.addEventListener("scroll", () => {
   navLinks.forEach(link => {
     link.classList.remove("active");
 
-    if (link.getAttribute("href") === "#" + current) {
+    if (link.getAttribute("data-target") === current) {
       link.classList.add("active");
     }
   });
